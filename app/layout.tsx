66280498114d9a2084eca9b8c1bdfdee314ee3e1
Vũ Trường Xuân <xuan.vt.1999@gmail.com>
2025-04-25 00:16:16 +0700

    chore: personalize title & description

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,9 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  title: 'Nim - Personal website template',
+  title: 'Shiro portfolio',
   description:
-    'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+    'Shiro\'s portfolio site powered by Nim',
 }
 
 const geist = Geist({
